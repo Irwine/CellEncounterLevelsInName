a26b9b846ae86e35b331602b579792ae02595453
Irwine <vandevelde632@gmail.com>
2021-09-05 17:10:18 +0200

    Update Program.cs

--- CellEncounterLevelsInName/Program.cs
+++ CellEncounterLevelsInName/Program.cs
@@ -14,7 +14,7 @@ namespace CellEncounterLevelsInName
 {
     public static class Program
     {
-        private static Lazy<Settings> _settings = null!;
+        private static Lazy<Config> _settings = null!;
         public static Task<int> Main(string[] args)
         {
             return SynthesisPipeline.Instance
